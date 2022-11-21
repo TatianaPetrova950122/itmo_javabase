65ddd49a9d1deb093ce5dbf0362deaa643b93b4b
TatianaPetrova950122 <undertow95@mail.ru>
2022-11-21 04:24:49 +0300

    Lesson1

--- src/Lesson1/task5.java
+++ src/Lesson1/task5.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class task5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        int c = scanner.nextInt();
+
         System.out.println(scanner.nextInt());
         System.out.println(scanner.nextInt());
         System.out.println(scanner.nextInt());
