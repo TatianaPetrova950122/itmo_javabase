3c031adae2ffe48ab2004ea0647b657bd2c24b02
TatianaPetrova950122 <undertow95@mail.ru>
2022-11-21 04:47:49 +0300

    Lesson2

--- src/Lesson2/JediMasterKenobiScan.java
+++ src/Lesson2/JediMasterKenobiScan.java
@@ -4,5 +4,25 @@ public class JediMasterKenobiScan {
     public static void main(String[] args) {
         Kenobi kenobi = new Kenobi("Obi-Wan Kenobi", "male", true, 57L, Color.BEIGE, 1);
 
+        System.out.println(kenobilightsabers(kenobi, 1));
+        System.out.println(jediMasterDressColor(kenobi, Color.BLACK));
+    }
+
+    public static Boolean kenobilightsabers(Kenobi kenobi, Integer lightsabers) {
+        if (lightsabers != 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static Color jediMasterDressColor(Kenobi kenobi, Color dressedColor) {
+        if (dressedColor != Color.BEIGE) {
+            System.err.println("Not a jedi Master Kenobi");
+        } else {
+            System.out.println("Maybe this robe belongs to Master Kenobi");
+        }
+        return dressedColor;
+
     }
 }
